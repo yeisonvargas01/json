644d27b35a4e51f0f9bd24daf93df92ecbef0441
yeisonvargas01 <yeisonvargass27@gmail.com>
2023-11-04 00:42:28 -0500

    phenomenal

--- funciones.js
+++ funciones.js
@@ -1,25 +1,19 @@
-$(document).ready(function() {
-    $("a").on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
+function validarFormulario() {
+    var nombres = document.getElementById('nombres').value;
+    var apellidos = document.getElementById('apellidos').value;
+    var correo = document.getElementById('correo').value;
+  
+    if (nombres === '' || apellidos === '' || correo === '') {
+      alert('Por favor complete todos los campos');
+      return false; 
+    }
+  
+    return true; 
+  }
 
-            var hash = this.hash;
-
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-                window.location.hash = hash;
-            });
-        } 
-    });
-});
-
-
-$(document).ready(function() {
-    setTimeout(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 800);
-    }, 5000);
-});
-
+  document.getElementById('mi-enlace').addEventListener('click', function(event) {
+    event.preventDefault(); // 
+    alert('¡Has hecho clic en el enlace!');
+  });
+  
+  
